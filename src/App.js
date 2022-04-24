f9f238a7beeeb52ad3d8bc5ce1ec74016d9f769e
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-04-24 12:23:36 +0430

    fixed size grids

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Button from "@mui/material/Button";
+import UnderstandBreakPoints from "./components/UnderstandBreakPoints";
 
 function App() {
-	return <Button variant="contained">Hello world</Button>;
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+      <UnderstandBreakPoints />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles(() => {
+  return {
+    root: {
+      flexGrow: 1,
+      padding: "10px",
+      backgroundColor: "silver",
+      margin: "4px",
+    },
+    paper: {
+      padding: "10px",
+      textAlign: "center",
+    },
+  };
+});
